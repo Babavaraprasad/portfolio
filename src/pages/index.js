9f784efd7789b01d919d2a5b9ed6307232984c39
Babavaraprasad <bapi@itu.dk>
2025-09-19 15:25:50 +0200

    included project information in page and used image component in nextjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   const projects = [
     { title: 'ATM web application', description: 'Distribution of currency notes & coins based on User input amount', link: 'https://github.com/Babavaraprasad/July_Enalyzer_project' },
-    { title: 'Thesis', description: 'Description for project two', link: '#' },
-    { title: 'Research Project', description: 'Description for project three', link: '#' },
+    { title: 'Thesis', description: 'An Emprical Research conducted at Bispebjerg Hospital in collaboration with Center for IT and Medical Technology (CIMT), Region Hovedstaden', link: 'https://github.com/Babavaraprasad/Thesis_2023' },
+    { title: 'Chat Application', description: 'Developed chat application to ease the connection process for students based on courses, skills at various universities in Denmark. Also applied design thinking principles for designing prototype', link: 'https://github.com/Babavaraprasad/Group5_TechnicalInteractionDesign' },
   ];
 
   return (
