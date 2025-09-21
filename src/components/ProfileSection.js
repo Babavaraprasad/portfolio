355ad316bb200d1848ef718704fdab7439ac239c
Babavaraprasad <bapi@itu.dk>
2025-09-21 13:11:53 +0200

    props handling in react for profile section & object usage to store data regarding description and tags

--- src/components/ProfileSection.js
+++ src/components/ProfileSection.js
@@ -1,8 +1,11 @@
 import styles from '../styles/ProfileSection.module.scss';
 import Image from 'next/image';
 
-export default function ProfileSection({text}) {
-let name = text ? text : 'Hi, Im Babavaraprasad Reddy Pingili';
+export default function ProfileSection({text, tagA, tagB, tagC}) {
+const name = text ? text : 'Hi, Im Babavaraprasad Reddy Pingili';
+const tagLeft = tagA ? tagA : "Frontend Developer";
+const tagCenter = tagB ? tagB : "React & C# Enthusiast";
+const tagRight =  tagC ? tagC : "Full stack Developer";
   return (
     <section className={styles.profileSection}>
         <Image
@@ -14,7 +17,7 @@ let name = text ? text : 'Hi, Im Babavaraprasad Reddy Pingili';
         >
         </Image>
       <h1>{name}</h1>
-      <p>Frontend Developer | React & C# Enthusiast | Full stack Developer</p>
+      <p> {tagLeft}| {tagCenter} | {tagRight} </p>
     </section>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,45 @@
-import styles from '../styles/ProfileSection.module.scss';
-import ProfileSection from '@/components/ProfileSection';
-import Card from '@/components/Card';
+import styles from "../styles/ProfileSection.module.scss";
+import ProfileSection from "@/components/ProfileSection";
+import Card from "@/components/Card";
 
 export default function About() {
+  const myData = {
+    name: "Babavaraprasad Reddy Pingili",
+    tagA: "M.S.c Software Design at ITU, Denmark",
+    tagB: "Full stack Developer",
+    tagC: "Honest & Good team player",
+    description: `I'm Baba, a master's graduate from IT University of Copenhagen in software design and development, 
+  specialized in Technical Interaction Design. By God's grace, pursuing my master's in Denmark has had a profoundly 
+  good effect on my life. Throughout my studies, I have acquired life skills such as Time Management, Team management, 
+  Public Speaking, Dealing with tough situations and bringing the best out, Communication and Collaboration, 
+  Presentations, and Documentation. 
+
+  I'm interested in applying my knowledge and shaping my career towards technology that makes human lives digitalized 
+  and sustainable, with the responsibility of leading an organisation 21 years down the line. 
+
+  Being a professional Software Developer with 5 years of experience, I enjoy writing clean code following industry 
+  standards and maintaining consistent build quality. My biggest strength is concentrating on writing easily readable 
+  code, delivering products with optimal performance, bug-free, and on time. I also make sure to conduct several 
+  testing techniques, such as Unit Testing, Manual Testing, Regression Testing, and Smoke Testing to assure accuracy 
+  of the developed software.`,
+  };
   return (
     <>
-    <div className={styles.AboutSection}>
-    <ProfileSection text={'Babavaraprasad Reddy Pingili'}/>
-    <Card 
-    width={'100%'}
-    height={'100%'}
-    description={'I work in a structured way, openly communicate with my team, and never shy away to ask for help whenever needed. I have a strong desire to work for the Denmark governance for better sustainable future for the citizens of the country and be part of the wonderful society.I work in a structured way, openly communicate with my team, and never shy away to ask for help whenever needed. I have a strong desire to work for the Denmark governance for better sustainable future for the citizens of the country and be part of the wonderful society.I work in a structured way, openly communicate with my team, and never shy away to ask for help whenever needed. I have a strong desire to work for the Denmark governance for better sustainable future for the citizens of the country and be part of the wonderful society.I work in a structured way, openly communicate with my team, and never shy away to ask for help whenever needed. I have a strong desire to work for the Denmark governance for better sustainable future for the citizens of the country and be part of the wonderful society.I work in a structured way, openly communicate with my team, and never shy away to ask for help whenever needed. I have a strong desire to work for the Denmark governance for better sustainable future for the citizens of the country and be part of the wonderful society.I work in a structured way, openly communicate with my team, and never shy away to ask for help whenever needed. I have a strong desire to work for the Denmark governance for better sustainable future for the citizens of the country and be part of the wonderful society.I work in a structured way, openly communicate with my team, and never shy away to ask for help whenever needed. I have a strong desire to work for the Denmark governance for better sustainable future for the citizens of the country and be part of the wonderful society.I work in a structured way, openly communicate with my team, and never shy away to ask for help whenever needed. I have a strong desire to work for the Denmark governance for better sustainable future for the citizens of the country and be part of the wonderful society.'} 
-    showLink = {false}
-    overflow = {true}
-     />
-    </div>
+      <div className={styles.AboutSection}>
+        <ProfileSection
+          text={myData.name}
+          tagA={myData.tagA}
+          tagB={myData.tagB}
+          tagC={myData.tagC}
+        />
+        <Card
+          width={"100%"}
+          height={""}
+          description={myData.description}
+          showLink={false}
+          overflow={true}
+        />
+      </div>
     </>
   );
 }
